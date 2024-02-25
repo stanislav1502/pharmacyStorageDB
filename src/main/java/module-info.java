155a86037324c2e611005653e2522e6ef81078bb
stanislav1502 <80790229+stanislav1502@users.noreply.github.com>
2024-02-25 16:38:20 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.magistraturasgi.pharmacystoragedb {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.magistraturasgi.pharmacystoragedb to javafx.fxml;
-    exports org.magistraturasgi.pharmacystoragedb;
-}
